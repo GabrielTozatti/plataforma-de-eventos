8903c2f5b314824173107ff1037a9b6b76b8c887
Gabriel Tozatti <gabrielbt@codebit.com.br>
2025-06-06 10:10:24 -0300

    Ajuste finos para rodar o build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,16 @@ import { Roboto } from 'next/font/google';
 import GlobalStyle from '@/styles/GlobalStyle';
 
 const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['200', '400', '500', '700', '800'],
-  variable: '--font-roboto',
+    subsets: ['latin'],
+    weight: ['200', '400', '500', '700', '800'],
+    variable: '--font-roboto',
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={{}}>
       <GlobalStyle />
-      <main className={roboto.variable}>
+      <main className={roboto.variable} suppressHydrationWarning>
         <Component {...pageProps} />
       </main>
     </ThemeProvider>
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-
     :root {
         --font-roboto: 'Roboto', sans-serif;
     }
